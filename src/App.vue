60f53bd9d73f70fb1a694e847c7a9d4929573541
veronj <veron_j@etna-alternance.net>
2018-07-23 21:20:34 +0200

    content

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue';
+
+export default {
+  data () {
+    return {
+      quotes: [
+        'The first quote',
+        'A longer quote this time'
+      ],
+      maxQuotes: 10
+    }
+  },
+  components: {
+      appQuoteGrid: QuoteGrid
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-sm-6 col-lg-3">
+            <div class="panel panel-default">
+                <div class="panel-body quote">
+                    <slot></slot>
+                </div>
+            </div>
+    </div>
+</template>
+
+<script>
+import Quote from './Quote.vue';
+    export default {
+        props: ['quotes'],
+        components: {
+            appQuote: Quote
+        }
+    }
+</script>
+
+<style>
+    .panel-body {
+        font-family: 'Arizonia', cursive;
+        font-size: 24px;
+        color: black;
+    }
+
+    .quote {
+        cursor: pointer;
+    }
+
+    .quote:hover {
+        background-color: burlywood;
+    }
+</style>
